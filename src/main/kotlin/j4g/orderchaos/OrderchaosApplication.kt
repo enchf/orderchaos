7b9242bd40523fd66b486eaf1b7c3a811fc86aba
Ernesto <e.ernesto.espinosa@gmail.com>
2021-09-30 05:08:59 -0500

    Add Spring dispatcher

---
+++ src/main/kotlin/j4g/orderchaos/OrderchaosApplication.kt
@@ -0,0 +1,11 @@
+package j4g.orderchaos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderchaosApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderchaosApplication>(*args)
+}
