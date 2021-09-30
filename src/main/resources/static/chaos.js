19e133ad755e79a9af310d31157b46c1cb1aece9
Ernesto <e.ernesto.espinosa@gmail.com>
2021-09-30 17:57:27 -0500

    Plotting results

--- src/main/resources/static/chaos.js
+++ src/main/resources/static/chaos.js
@@ -1,3 +1,23 @@
+var CHART_DOM = $('#chaosChart');
+var CHART_CONFIG = {
+     type: 'line',
+     data: { labels: [], datasets: [] },
+     options: {
+       responsive: true,
+       maintainAspectRatio: false,
+       plugins: {
+         legend: {
+           position: 'top',
+         },
+         title: {
+           display: true,
+           text: 'Logistic Equation'
+         }
+       }
+     },
+};
+var CHART = new Chart(CHART_DOM, CHART_CONFIG);
+
 function updatePlot() {
     var formData = ['ratio','x0','iterations']
         .map(id => id + '=' + $('#' + id).val())
@@ -6,5 +26,14 @@ function updatePlot() {
 }
 
 function updatePlotCallback(data) {
-    alert(data);
+    var labels = [...data.keys()];
+    var chartData = {
+        labels: labels,
+        datasets: [{ label: 'Population ratio over time', data: data,
+                     borderColor: 'rgb(255, 99, 132)',
+                     backgroundColor: 'rgb(255, 99, 132, 0.5)' }]
+    };
+
+    CHART.data = chartData;
+    CHART.update();
 }
